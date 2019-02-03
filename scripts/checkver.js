5574fd0c3215014df6a0f02f16eed590a6e28b9f
username1565 <39200328+username1565@users.noreply.github.com>
2019-02-03 14:30:47 +0200

    Update version
    1. Update date in /pages/version.txt
    2. Add working link to version.txt in /scripts/checkver.js

--- scripts/checkver.js
+++ scripts/checkver.js
@@ -2,7 +2,8 @@
 	Checks if version in repository differs from current
 */
 
-var _repoVersion = 'https://raw.githubusercontent.com/nanoboard/nanoboard/feature/2.0/bin/Debug/pages/version.txt';
+//var _repoVersion = 'https://raw.githubusercontent.com/nanoboard/nanoboard/feature/2.0/bin/Debug/pages/version.txt';	//old not working link
+var _repoVersion = 'https://raw.githubusercontent.com/username1565/nanoboard/master/pages/version.txt';					//new working link
 var _buildVersion = '../pages/version.txt';
 
 function checkVersion() {
